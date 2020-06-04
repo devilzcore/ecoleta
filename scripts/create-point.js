10267562af65f2713bc428ac291a1edb80a3da8e
devilzcore <aldosilva1@live.com>
2020-06-04 02:20:32 -0300

    fix field when selecting the state the city field does not clear

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -25,11 +25,14 @@ function getCities(event) {
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disable = true
+
     fetch(url)
     .then( res => res.json() )
     .then( cities => {
         for( city of cities) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
 
         citySelect.disable = false
